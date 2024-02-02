cf4e4c58c8222d0a6d03371557600d18cb6d4813
Mak Moinee <titus.silver.ph@gmail.com>
2024-02-02 16:41:37 +0800

    update code

--- app/Http/Controllers/AdminDetectionsController.php
+++ app/Http/Controllers/AdminDetectionsController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use DateTime;
+use DateTimeZone;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class AdminDetectionsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         if (session()->exists("users")) {
             $mUser = session()->pull("users");
@@ -17,10 +19,24 @@ class AdminDetectionsController extends Controller
             if ($userType != 1) {
                 return redirect("/");
             }
-
+            $searchKey = $request->query('search');
             $queryResult = DB::table("vwresults")->where('name','<>','unknown')->get();
             $detections = json_decode($queryResult, true);
-            return view("admin.detections", ['detections' => $detections, 'searchKey' => '']);
+            $tmpDetection = array();
+            foreach($detections as $d){
+                $dateTime = new DateTime($d['detectedDate'], new DateTimeZone('UTC'));
+                $dateTime->setTimezone(new DateTimeZone('Asia/Manila'));
+                $formattedDate = $dateTime->format('Y-m-d h:i A');
+                $d['detectedDate'] = $formattedDate;
+                if ($searchKey) {
+                    if (str_contains($searchKey, $d['room'])) {
+                        array_push($tmpDetection, $d);
+                    }
+                } else {
+                    array_push($tmpDetection, $d);
+                }
+            }
+            return view("admin.detections", ['detections' => $tmpDetection, 'searchKey' => '']);
         } else {
             return redirect("/");
         }
